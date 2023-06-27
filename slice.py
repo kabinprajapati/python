59488d36fd7be68ac2a324c97b94960ca3a05833
kabin <kabinprajapati@gmail.com>
2023-06-27 20:05:02 +0900

    slice.py added

---
+++ slice.py
@@ -0,0 +1,25 @@
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+print(cars)
+
+# Print out drives_right value of Morocco
+print(cars.iloc[5,2])
+
+
+# Print sub-DataFrame
+print(cars.iloc[[4,5],[1,2]])
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+print(cars)
+
+# Print out drives_right column as Series
+print(cars.iloc[:,2])
+
+# Print out drives_right column as DataFrame
+print(cars.iloc[:,[2]])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.iloc[:,[0,2]])
